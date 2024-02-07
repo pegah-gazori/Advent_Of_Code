934909cb0ca54b4fc922c8be5647d61ce83ee964
Pegah Gazori <gazori.pegah@gmail.com>
2024-02-08 00:11:23 +0330

    AOC-1: Enhanced the codes of Day 2 + Added automatic data retrieval and applied relevant configurations.

--- 2020/source/day_two.py
+++ 2020/source/day_two.py
@@ -4,42 +4,28 @@
 PROBLEM_URL = "https://adventofcode.com/2020/day/2"
 
 
-def parse_data(data_path):
-    """Parsing data from a text file."""
-
-    with open(data_path) as file:
-        dict_data = {key: value for line in file for(key, value) in [line.strip().split(": ")]}
-    return dict_data
+from aocd import get_data
 
 
-def extracting_policy_elements(policy):
-    """Extracting the elements of policy from the key of the data dictionary"""
+def validate_password():
 
-    char_and_limit_split = policy.split(" ")
-    limit_split = char_and_limit_split[0].split("-")
+    total_number_of_valid_passwords = 0
+    data = get_data(day=2, year=2020).split("\n")
 
-    min_char_num = int(limit_split[0])
-    max_char_num = int(limit_split[1])
-    char = char_and_limit_split[1]
+    for row in data:
+        elements = row.split()
+        character_range = elements[0].split("-")
+        minimum_character_count = int(character_range[0])
+        maximum_character_count = int(character_range[1])
+        character_to_count = elements[1].replace(":", "")
+        password = elements[2]
 
-    return min_char_num, max_char_num, char
+        occurrences = int(password.count(character_to_count))
+        if minimum_character_count <= occurrences <= maximum_character_count:
+            total_number_of_valid_passwords += 1
 
-
-def validate_password(input_data):
-    """This function validates password based on their policies, and it returns the total number of valid passwords."""
-
-    total_number_of_valid_password = 0
-
-    for key, value in input_data.items():
-        min_of_character, max_of_character, character_to_count = extracting_policy_elements(policy=key)
-        occurrences = value.count(character_to_count)
-        if min_of_character <= occurrences <= max_of_character:
-            total_number_of_valid_password += 1
-
-    return total_number_of_valid_password
+    return total_number_of_valid_passwords
 
 
 if __name__ == "__main__":
-    parsed_data = parse_data(data_path="../data/day_two_data.txt")
-    number_of_valid_passwords = validate_password(input_data=parsed_data)
-    print(f"Number of valid passwords: {number_of_valid_passwords}")
+    print("Number of Valid Passwords: ", validate_password())
